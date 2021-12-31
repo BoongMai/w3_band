dc490e7db7ab532c3b845aad4478900e325231ff
BoongMai <thanhduypham9@gmail.com>
2021-12-31 21:18:27 +0700

    Create Header

---
+++ assets/JS/main.js
@@ -0,0 +1,30 @@
+// var listMenuNode = document.getElementById('nav')
+// console.log(listMenuNode.querySelectorAll('li>a')[2]);
+
+// querySelector và ByID trả về element
+
+// var element = document.getElementById('main')
+// console.log(element);
+// var element2 = document.querySelector('div>ul>li')
+// console.log(element2);
+
+/* querySelectorAll sẽ cho ra một NoteList, 
+ chúng ta có thể lấy đc elenment từ chỉ mục index */
+
+//  var boxElement = document.querySelectorAll('div')[]
+//  console.log(boxElement);
+
+/*ByClassName thì cho ra một HTMLCollection,
+ta có thể lấy ra element bằng cách truyền giá trị index.
+*/
+// var boxElement2 = document.getElementsByClassName('box')
+// console.log(boxElement2);
+
+
+
+
+// var boxElement = document.getElementsByClassName('box')[0]
+// console.log(boxElement);
+
+// var childrenElements =
+// console.log(boxElement.getElementsByClassName('children')[1]);
