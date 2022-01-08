b1d4d0a9b66e88ed2a3721b593ab1e5eeaa5058a
BoongMai <thanhduypham9@gmail.com>
2022-01-08 12:40:55 +0700

    hoàn thành phần map

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -28,3 +28,38 @@ ta có thể lấy ra element bằng cách truyền giá trị index.
 
 // // var childrenElements =
 // console.log(boxElement.getElementsByClassName('children')[1]);
+
+const buyBtns = document.querySelectorAll('.js-buy-btn');
+const model = document.querySelector('.js-modal')
+const modelClose = document.querySelector('.js-close-modal')
+const modelContaniner = document.querySelector('.js-modal-contaniner')
+
+function showModal(){
+    model.classList.add('open')
+}
+
+function closeModal() {
+    model.classList.remove('open')
+}
+
+
+for (const buyBtn of buyBtns){
+    buyBtn.addEventListener('click', showModal)
+}
+
+modelClose.addEventListener('click', closeModal)
+
+model.addEventListener('click', closeModal)
+
+
+// event là một sự kiện, stopPropagation là dửng nổi bọt đến các phần tử bên trong
+modelContaniner.addEventListener('click', function(event){
+    event.stopPropagation()
+})
+/**
+ * Truy suất vào nơi cần nhận sự kiện sau đó tạo vòng lập để lấy từng element Node
+ * riêng lẽ và gán vào giá trị mới, sau đó dùng giá trị mới với từng element
+ * riêng lẽ để dùng  hàm addEventListener.
+ * -
+ */
+// console.log(buyBtns);
